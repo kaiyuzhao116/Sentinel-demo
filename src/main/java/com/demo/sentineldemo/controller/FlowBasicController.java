b95dd21d088959a3e4575e448c92b75a1cc914ff
kaiyu <2628285602@qq.com>
2026-03-24 18:26:40 +0800

    流控模式 - 关联模式

--- src/main/java/com/demo/sentineldemo/controller/FlowBasicController.java
+++ src/main/java/com/demo/sentineldemo/controller/FlowBasicController.java
@@ -1,5 +1,7 @@
 package com.demo.sentineldemo.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class FlowBasicController {
     @GetMapping("/hello")
     public String hello() {
-
-
         return "Hello Sentinel! 基础限流测试成功";
+    }
+    // 资源1：下单接口（会被关联限流）
+    @SentinelResource(value = "order", blockHandler = "orderBlockHandler")
+    @GetMapping("/order")
+    public String order() {
+        return "下单成功 ✅";
+    }
+
+    // 下单接口被限流时的降级处理
+    public String orderBlockHandler(BlockException e) {
+        return "下单失败 ❌：支付接口压力过大，暂时限制下单";
+    }
+
+    // 资源2：支付接口（关联资源）
+    @SentinelResource(value = "pay")
+    @GetMapping("/pay")
+    public String pay() {
+        return "支付成功 ✅";
     }
 
 
